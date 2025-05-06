f150adbf5182a882b321a487ff255187c1a172d5
arosyihuddin <arosyihuddin6@gmail.com>
2025-05-06 16:48:39 +0700

    first init

---
+++ examples/async_search.py
@@ -0,0 +1,25 @@
+from searxng_wrapper import SearxngWrapper
+
+
+async def main():
+    client = SearxngWrapper(
+        base_url="https://search.arosyihuddin.site",
+        log_level="DEBUG"
+    )
+
+    result = await client.asearch(
+        q="Ahmad Rosyihuddin",
+        language="all",
+        categories="general",
+        safesearch="off",
+        page=1,
+        time_range="month",
+        enabled_engines=["bing__general"],
+        disabled_engines=["google__general", "brave__general",
+                          "duckduckgo__general", "qwant__general", "startpage__general"]
+    )
+    print(result)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
---
+++ examples/search.py
@@ -0,0 +1,21 @@
+from searxng_wrapper import SearxngWrapper
+
+if __name__ == "__main__":
+    client = SearxngWrapper(
+        base_url="https://search.arosyihuddin.site",
+        log_level="DEBUG"
+    )
+
+    result = client.search(
+        q="meta",
+        language="auto",
+        categories="social media",
+        safesearch="off",
+        page=1,
+        time_range="all",
+        enabled_engines=["bing__general"],
+        disabled_engines=["google__general", "brave__general",
+                          "duckduckgo__general", "qwant__general", "startpage__general"]
+    )
+
+    print(result)
